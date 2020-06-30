ac8ca65e227e8216dc22bd611cb1ec2d776eb62d
samirboulgant <67659734+samirboulgant@users.noreply.github.com>
2020-06-30 22:44:57 +0100

    Add files via upload

---
+++ sampler/gallery.js
@@ -0,0 +1,18 @@
+function hello(){
+    document.getElementById('ho').style.color="black";
+    document.getElementById('ha').style.color="#C70039";
+    document.getElementById('hi').style.color="#C70039";
+    
+}
+function helloaa(){
+    document.getElementById('ha').style.color="black";
+    document.getElementById('ho').style.color="#C70039";
+    document.getElementById('hi').style.color="#C70039";
+    
+}
+function helloii(){
+    document.getElementById('hi').style.color="black";
+    document.getElementById('ha').style.color="#C70039";
+    document.getElementById('ho').style.color="#C70039";
+    
+}
